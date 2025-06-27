e8b9d0522601fc62b978d6b5eaec3a2ab7b19cae
yash24shukla <thisform240@gmail.com>
2025-06-28 03:41:49 +0530

    completed the how it works section

---
+++ frontend/src/constants/index.js
@@ -0,0 +1,10 @@
+export const features = [{ 
+    title : "Enter The Temple",
+    desc : "Sign up using your institutional email and get your scriptum arcanum"
+},{ 
+    title : "Seek the Holy Grail",
+    desc : "Explore the distilled wisdom of enlightened minds through their shared notes."
+},{ 
+    title : "Offer your knowledge",
+    desc : "Share your wisdom with the world and be rewarded with recognition."
+}]
